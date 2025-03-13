b5be68351e93521a07de843349ed3b4803250aeb
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2025-03-13 18:36:03 -0400

    Update Server.java

--- Server.java
+++ Server.java
@@ -5,14 +5,15 @@ package PeerToPeerProject366;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.IOException;
 
 public class Server {
     private static final int PORT = 5000;
-    private static Set<String> registeredClients = new HashSet<>();
+    private static Map<String, ClientInfo> registeredClients = new HashMap<>();
 
-   static class ClientInfo {
+    static class ClientInfo {
         String name;
         String role;
         String ipAddress;
@@ -28,29 +29,71 @@ public class Server {
         }
     }
 
-    public static void main(String[] args) {
-        try (DatagramSocket socket = new DatagramSocket(PORT)) {
-            byte[] buffer = new byte[1024];
-            System.out.println("Server is running...");
+    public static void main(String[] args) throws IOException {
+        DatagramSocket ds = new DatagramSocket(PORT);
+        byte[] receive = new byte[65535];
+        DatagramPacket DpReceive = null;
 
-            while (true) {
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-                socket.receive(packet);
+        System.out.println("Server is running...");
 
-                String message = new String(packet.getData(), 0, packet.getLength());
-                InetAddress clientAddress = packet.getAddress();
-                int clientPort = packet.getPort();
+        while (true) {
+            DpReceive = new DatagramPacket(receive, receive.length);
+            ds.receive(DpReceive);
 
-                if (message.startsWith("REGISTER")) {
-                    registeredClients.add(clientAddress.toString() + ":" + clientPort);
-                    System.out.println("Registered: " + clientAddress + ":" + clientPort);
-                } else if (message.startsWith("DEREGISTER")) {
-                    registeredClients.remove(clientAddress.toString() + ":" + clientPort);
-                    System.out.println("Deregistered: " + clientAddress + ":" + clientPort);
+            String message = data(receive).toString();
+            InetAddress clientAddress = DpReceive.getAddress();
+            int clientPort = DpReceive.getPort();
+
+            System.out.println("Received message: " + message);
+
+            String[] parts = message.split(" \\| ");
+            String command = parts[0];
+            String rqNumber = parts[1];
+
+            if (command.equals("REGISTER")) {
+                String name = parts[2];
+                String role = parts[3];
+                String ipAddress = parts[4];
+                int udpPort = Integer.parseInt(parts[5]);
+                int tcpPort = Integer.parseInt(parts[6]);
+
+                if (registeredClients.containsKey(name)) {
+                    String response = "REGISTER-DENIED | " + rqNumber + " | Name already in use";
+                    DatagramPacket responsePacket = new DatagramPacket(response.getBytes(), response.length(), clientAddress, clientPort);
+                    ds.send(responsePacket);
+                    System.out.println("Registration denied for: " + name);
+                } else {
+                    registeredClients.put(name, new ClientInfo(name, role, ipAddress, udpPort, tcpPort));
+                    String response = "REGISTERED | " + rqNumber;
+                    DatagramPacket responsePacket = new DatagramPacket(response.getBytes(), response.length(), clientAddress, clientPort);
+                    ds.send(responsePacket);
+                    System.out.println("Registered: " + name);
                 }
+            } else if (command.equals("DE-REGISTER")) {
+                String name = parts[2];
+                registeredClients.remove(name);
+                System.out.println("Deregistered: " + name);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+
+            // clear buffer
+            receive = new byte[65535];
+
+            if (message.equals("bye")) {
+                System.out.println("Client sent bye.....EXITING");
+                break;
+            }
         }
+    }
+
+    // convert byte array data into a string representation
+    public static StringBuilder data(byte[] a) {
+        if (a == null) return null;
+        StringBuilder ret = new StringBuilder();
+        int i = 0;
+        while (i < a.length && a[i] != 0) {
+            ret.append((char) a[i]);
+            i++;
+        }
+        return ret;
     }
 }
