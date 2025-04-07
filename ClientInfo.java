31d1c56f8a479c86c62349c2d7ac4c712a5ed843
Nihal Islam <123002292+ImNotNihal@users.noreply.github.com>
2025-04-07 10:57:50 -0400

    Create ClientInfo.java

---
+++ ClientInfo.java
@@ -0,0 +1,21 @@
+public class ClientInfo {
+    public final String name;
+    public final String role;
+    public final String ipAddress;
+    public final int udpPort;
+    public final int tcpPort;
+
+    public ClientInfo(String name, String role, String ipAddress, int udpPort, int tcpPort) {
+        this.name = name;
+        this.role = role;
+        this.ipAddress = ipAddress;
+        this.udpPort = udpPort;
+        this.tcpPort = tcpPort;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Client[name=%s, role=%s, ip=%s, udp=%d, tcp=%d]",
+                name, role, ipAddress, udpPort, tcpPort);
+    }
+}
